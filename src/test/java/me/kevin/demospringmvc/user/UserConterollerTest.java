7eeb05c97ed35d93643a18bcf46ae4d910521015
jiwonjung <4dakto@gmail.com>
2020-05-02 16:03:09 +0900

    init.

---
+++ src/test/java/me/kevin/demospringmvc/user/UserConterollerTest.java
@@ -0,0 +1,47 @@
+package me.kevin.demospringmvc.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+public class UserConterollerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("hello"));
+    }
+
+    @Test
+    public void createUser_JSON() throws Exception {
+        String userJson = "{\"username\":\"kevin\", \"password\":\"123\"}";
+        mockMvc
+                .perform(MockMvcRequestBuilders.post("/users/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(userJson))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username",
+                        is(equalTo("kevin"))))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password",
+                        is(equalTo("123"))));
+
+    }
+}
